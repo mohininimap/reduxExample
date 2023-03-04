a6dc7733ea1e6e598f3f1519df3bb75627079d91
mohininimap <mohiniraut@nimapinfotech.com>
2023-03-04 17:31:52 +0530

    redux example done

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const Navbar = () => {
+  const amount=useSelector(state=>state.amount)
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+      
+      </ul>
+      <div>
+        <button  className='btn-primary'>YOur balace : {amount}</button>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from './state/index';
+const Shop = () => {
+  const dispatch=useDispatch();
+  return (
+    <div>
+        <h2>Deposit / Withdraw Money</h2>
+        <button className='btn btn-primary mx-2' onClick={()=>dispatch(actionCreators.withdrawMoney(50))} >-</button>
+        Update Balance
+        <button className='btn btn-primary mx-2' onClick={()=>dispatch(actionCreators.depositMoney(100))}>+ </button>
+    </div>
+  )
+}
+
+export default Shop
